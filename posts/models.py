7d291bb7a8240a912fdc1de1f5a857942e97f5a8
Sergei Khortiuk <wertigo2@ya.ru>
2020-04-10 00:30:44 +0300

    Sprint 3 results

---
+++ posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField('title', max_length=200)
+    slug = models.SlugField('slug', max_length=50, primary_key=True)
+    description = models.TextField('description')
+
+    def __str__(self):
+        return '/' + self.slug + '/ ' + self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='author_posts')
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL, blank=True, null=True, related_name='group_posts')
